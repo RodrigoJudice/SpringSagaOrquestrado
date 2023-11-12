101bba46d27b976b0c800d284ac098e7441021b3
Judice <r_judice@msn.com>
2023-11-12 09:24:24 -0300

    Orchestrator-service - init

---
+++ orchestrator-service/src/test/java/com/git/judice/orchestrator/OrchestratorServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.git.judice.orchestrator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrchestratorServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
