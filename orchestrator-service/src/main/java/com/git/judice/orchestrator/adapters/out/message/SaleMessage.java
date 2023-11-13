bdade128f66134b3aeb40b67532a22aed2e4a2de
Judice <r_judice@msn.com>
2023-11-13 08:30:20 -0300

    Ajuste SaleMessage - erro no contrutor

--- orchestrator-service/src/main/java/com/git/judice/orchestrator/adapters/out/message/SaleMessage.java
+++ orchestrator-service/src/main/java/com/git/judice/orchestrator/adapters/out/message/SaleMessage.java
@@ -2,18 +2,17 @@ package com.git.judice.orchestrator.adapters.out.message;
 
 import com.git.judice.orchestrator.application.core.domain.Sale;
 import com.git.judice.orchestrator.application.core.domain.enums.SaleEvent;
-
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+@Builder
 @Data
-@NoArgsConstructor
 @AllArgsConstructor
+@NoArgsConstructor
 public class SaleMessage {
 
   private Sale sale;
-
   private SaleEvent event;
-
 }
--- payment-service/src/main/java/com/git/judice/payment/adapters/out/message/SaleMessage.java
+++ payment-service/src/main/java/com/git/judice/payment/adapters/out/message/SaleMessage.java
@@ -2,18 +2,17 @@ package com.git.judice.payment.adapters.out.message;
 
 import com.git.judice.payment.application.core.domain.Sale;
 import com.git.judice.payment.application.core.domain.enums.SaleEvent;
-
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+@Builder
 @Data
-@NoArgsConstructor
 @AllArgsConstructor
+@NoArgsConstructor
 public class SaleMessage {
 
-    private Sale sale;
-
-    private SaleEvent event;
-
+  private Sale sale;
+  private SaleEvent event;
 }
